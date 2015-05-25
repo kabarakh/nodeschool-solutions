2dd30ecc7d4498f2d51fd0dc1e88191191ab4ddc
Kabarakh <git@kabarakh.de>
2015-05-26 00:44:04 +0200

    ADD promise-it-wont-hurt challenge 8 (an important rule)

--- promise-it-wont-hurt/08_an_important_rule.js
+++ promise-it-wont-hurt/08_an_important_rule.js
@@ -6,3 +6,26 @@ var args = process.argv;
 var q = require('q');
 
 //** an_important_rule **//
+var throwMyGod = function () {
+	throw Error('OH NOES');
+};
+
+var iterate = function (numberToIterate) {
+	console.log(numberToIterate);
+	return (numberToIterate + 1);
+};
+
+q.fcall(iterate, 1)
+	.then(iterate)
+	.then(iterate)
+	.then(iterate)
+	.then(iterate)
+	.then(throwMyGod)
+	.then(iterate)
+	.then(iterate)
+	.then(iterate)
+	.then(iterate)
+	.then(iterate)
+	.then(null, console.log);
+
+
